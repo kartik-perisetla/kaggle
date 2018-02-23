ad5eea2ccc7e9a496b31c32990daf6e46ee2a96d
Soumya Batra <sobatr@microsoft.com>
2018-02-23 02:39:13 -0800

    Added TfIdf Vectorizer, using Linear SVC with best parameters, accuracy = 96-99%

--- ToxicCommentClassification/final.py
+++ ToxicCommentClassification/final.py
@@ -7,6 +7,7 @@ import copy
 from ast import literal_eval
 from collections import Counter
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_predict
 from sklearn.linear_model import LinearRegression
@@ -15,6 +16,7 @@ from sklearn import metrics
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from sklearn.calibration import CalibratedClassifierCV
 
 CHAR_REP = 10
 TRAIN_KEY = 'TRAIN'
@@ -69,32 +71,38 @@ def get_binary_dict(text, tolerance=0):
     count_dict = get_count_dict(text)
     return {key:1 if val > tolerance else 0 for key,val in count_dict.items()}
 
-# TODO: implement - get tf dict for input text
-def get_term_freq_dict(text, tolerance=0):
-    pass
+def get_tf_idf_dict(text, tolerance=5):
+    tf_vec = TfidfVectorizer(min_df=tolerance)
+    return tf_vec.fit_transform(text), tf_vec
 
-def extract_features(list_of_text, vectorizer= DictVectorizer()):
-    return vectorizer.fit_transform([get_binary_dict(text) for text in list_of_text]), vectorizer
+def extract_features(list_of_text, vectorizer= DictVectorizer(), is_binary=True):
+    if is_binary:
+        return vectorizer.fit_transform([get_binary_dict(text) for text in list_of_text]), vectorizer
+    else:
+        return get_tf_idf_dict(list_of_text)
 
 def extract_test_features(list_of_text, vectorizer):
-    return vectorizer.transform([get_binary_dict(text) for text in list_of_text])
+    # return vectorizer.transform([get_binary_dict(text) for text in list_of_text])
+    return vectorizer.transform(list_of_text)
     
 def train_on_features(class_label, train_features, train_labels, model_skeleton):
     model = copy.copy(model_skeleton)
-    print("about to fit model:'"+model.estimator.__class__.__name__+"' on " + str(len(train_labels)) + " instances")
+    print("about to fit model:'"+model.__class__.__name__+"' on " + str(len(train_labels)) + " instances")
     model.fit(train_features, train_labels)
-    print("fitting complete on model:'"+model.estimator.__class__.__name__+"' on " + str(len(train_labels)) + " instances")
+    print("fitting complete on model:'"+model.__class__.__name__+"' on " + str(len(train_labels)) + " instances")
 
     print("\n")
-    print("*"*CHAR_REP + model.estimator.__class__.__name__ + "*"*CHAR_REP )
+    print("*"*CHAR_REP + model.__class__.__name__ + "*"*CHAR_REP )
     # print model params
-    print("\nBest params for '"+ model.estimator.__class__.__name__ + "' for class '" + class_label + "' => " + str(model.best_params_))
+    # print("\nBest params for '"+ model.__class__.__name__ + "' for class '" + class_label + "' => " + str(model.best_params_))
 
     # commenting -> no need now as we are doing parameter sweep
-    print("Cross-Validation Accuracy for '"+ model.estimator.__class__.__name__ + "' for class '" + class_label + "' => ")
+    print("Cross-Validation Accuracy for '"+ model.__class__.__name__ + "' for class '" + class_label + "' => ")
 
     cross_validation_score = cross_validate(model, train_features, train_labels)
     print(cross_validation_score)
+
+    cross_validation_score = 1
 
     # Save the best model for the class label based on CV accuracy
     best_model_accuracy = best_model_dict.get(class_label, (0, None, None))
@@ -105,7 +113,7 @@ def train_on_features(class_label, train_features, train_labels, model_skeleton)
     return model
 
 
-def get_features(text_list, key=None):
+def get_features(text_list, key=None, is_binary=True):
     """Method to get features for input list of text
     
     Arguments:
@@ -118,7 +126,7 @@ def get_features(text_list, key=None):
         - pass None as key when actually want to invoke feature extraction method
     """    
     if key is None:
-        train_features, vectorizer = extract_features(text_list)
+        train_features, vectorizer = extract_features(text_list, is_binary=is_binary)
         add_to_cache(BINARY_VECTORIZER_KEY, vectorizer)
         return (train_features, vectorizer)
     else:
@@ -128,7 +136,7 @@ def get_features(text_list, key=None):
             train_features, vectorizer = item
         else:
             # extract features using input text_list
-            train_features, vectorizer = extract_features(text_list)
+            train_features, vectorizer = extract_features(text_list, is_binary=is_binary)
 
             # cache the vectorizer and train features for dataset identified with a key
             add_to_cache(BINARY_VECTORIZER_KEY, vectorizer)
@@ -145,7 +153,7 @@ def train_on_file(train_file_name, model_skeleton):
     print("about to extract features")
     # train_features, vectorizer = extract_features(text_list)
 
-    train_features, vectorizer = get_features(text_list, TRAIN_KEY)
+    train_features, vectorizer = get_features(text_list, TRAIN_KEY, False)
 
     print("extracted features")
 
@@ -176,6 +184,7 @@ def test_on_file(test_file_name, model_collection, vectorizer):
     test_data = read_file(test_file_name)
     ids = [dat[0] for dat in test_data]
     text_list = [dat[1] for dat in test_data]
+    print("test_on_file:about to extract features for test instances")
     test_features = extract_test_features(text_list, vectorizer)
     test_probabilities = []
     for model in model_collection:
@@ -184,7 +193,7 @@ def test_on_file(test_file_name, model_collection, vectorizer):
     return ids, list(zip(*test_probabilities))
     
 def write_to_submission_file(filepath, ids, results):
-    writo = [get_from_cache(CLASS_LABELS_KEY)]
+    writo = [['id'] + get_from_cache(CLASS_LABELS_KEY)]
     with open(filepath, "wt", encoding="utf-8", newline='') as fop:
         writer = csv.writer(fop)       
         for idx, item in zip(ids, results):
@@ -231,6 +240,7 @@ def run(args):
 
     models = []
     scores = ['precision', 'recall', 'accuracy']
+    scores = ['accuracy']
 
     skip_train_flag = literal_eval(args[3])
 
@@ -260,18 +270,21 @@ def run(args):
         #TODO : use term-frequency dict vectorizer and try all these algorithms
 
         for score in scores:
-            models = [
-                        # GridSearchCV(estimator=LinearRegression(), param_grid=linear_params, cv=5, scoring='%s_macro' % score, verbose=True),
+            # models = [
+            #             # GridSearchCV(estimator=LinearRegression(), param_grid=linear_params, cv=5, scoring='%s_macro' % score, verbose=True),
 
-                        GridSearchCV(estimator=LogisticRegression(dual = False, class_weight='balanced'), param_grid=lr_params, cv=5, scoring='%s_macro' % score, verbose=True),
+            #             GridSearchCV(estimator=LogisticRegression(dual = False, class_weight='balanced'), param_grid=lr_params, cv=5, scoring='%s_macro' % score, verbose=True),
 
-                        GridSearchCV(estimator=svm.LinearSVC(), param_grid=svm_params, cv=5, scoring='%s_macro' % score, verbose=True),
+            #             GridSearchCV(estimator=svm.LinearSVC(), param_grid=svm_params, cv=5, scoring='%s_macro' % score, verbose=True),
                         
-                        GridSearchCV(estimator=BernoulliNB(), param_grid=nb_params , cv=5, scoring='%s_macro' % score, verbose=True)
-                    ]
+            #             GridSearchCV(estimator=BernoulliNB(), param_grid=nb_params , cv=5, scoring='%s_macro' % score, verbose=True)
+            #         ]
+
+            models = [CalibratedClassifierCV(svm.LinearSVC(C=1))]
 
             for model_skeleton in models:
-                print("Training '" + model_skeleton.estimator.__class__.__name__ + "' on file")
+                # print("Training '" + model_skeleton.estimator.__class__.__name__ + "' on file")
+                print("Training '" + model_skeleton.__class__.__name__ + "' on file")
                 model_collection, vectorizer = train_on_file(args[0], model_skeleton)
         
         best_model_collection = []
@@ -299,9 +312,9 @@ def run(args):
 
 if __name__ == '__main__':
     change_to_curdir()
-    if len(sys.argv) >= 4:
+    if len(sys.argv) >= 5:
         print("launched with args:" + str(sys.argv))
         run(sys.argv[1:])
     else:
         print("Must pass atleast 3 params.")
-        print("python final.py <train_file_name> <test_file_name> <output_file_name>")
+        print("python final.py <train_file_name> <test_file_name> <output_file_name> <skip_train_flag>")
